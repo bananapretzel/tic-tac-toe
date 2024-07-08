983589a326b1f4d304208afec18d74127c610086
Casey Cotton <kc.cotton@windowslive.com>
2024-07-09 07:52:54 +1200

    Created a method which translates the div being clicked and changing the gameboard to represent the change.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const tiles = document.querySelectorAll(".tile");
+const whosTurnDisplay = document.querySelector("h2");
 
 const gameBoard = (function () {
      const tiles = [[0, 1, 2],
@@ -9,7 +10,7 @@ const gameBoard = (function () {
      [" ", " ", " "]];
      const getTiles = () => tiles;
      const getPlacedTiles = () => placedTiles;
-     const placeTile = (index, symbol) => placedTiles[index+1] = symbol;
+     const placeTile = (position, symbol) => placedTiles[position[0]][position[1]] = `${symbol}`;
      return { getTiles, getPlacedTiles, placeTile };
 })();
 
@@ -57,8 +58,8 @@ function consoleDisplayBoard() {
 
 //playGame(playerOne, playerTwo);
 
-const playerOne = createPlayer("Jack");
-const playerTwo = createPlayer("Jill"); 
+const playerOne = createPlayer("Player One");
+const playerTwo = createPlayer("Player Two"); 
 playerOne.setSymbol("X");
 playerTwo.setSymbol("O"); 
 let whosTurn = playerOne;
@@ -73,6 +74,39 @@ function playGame() {
 
 }
 
+function checkGameState() {
+     const tileArray = gameBoard.getPlacedTiles();
+     const firstRow = new Set(tileArray[0])
+     if (firstRow.size === 1 && !(firstRow.has(" ") || firstRow.has("_"))) {
+          console.log("win");
+          
+
+     }
+}
+
+function translate(tile) {
+     let arrayPosition = [];
+     const classNames = tile.className;
+     if (classNames.includes("row-one")) {
+          arrayPosition.push(0);
+     } else if (classNames.includes("row-two")) {
+          arrayPosition.push(1);
+     } else if (classNames.includes("row-three")) {
+          arrayPosition.push([2]);
+     }
+
+     if (classNames.includes("col-one")) {
+          arrayPosition.push(0);
+     } else if (classNames.includes("col-two")) {
+          arrayPosition.push(1);
+     } else if (classNames.includes("col-three")) {
+          arrayPosition.push(2);
+     }
+     //console.log(arrayPosition);
+     return arrayPosition;
+     
+
+}
 
 
 for (let tile of tiles) {
@@ -81,9 +115,15 @@ for (let tile of tiles) {
           if (tile.textContent === "") {
                tile.textContent = `${whosTurn.getSymbol()}`;
                whosTurn = whosTurn === playerOne ? playerTwo : playerOne;
+               whosTurnDisplay.textContent = `${whosTurn.name}'s turn`;
+               console.log(translate(this));
+               gameBoard.placeTile(translate(this), whosTurn.getSymbol());
+               checkGameState();
+
           }
           
           console.log("click");
+          
      })
 }
 
